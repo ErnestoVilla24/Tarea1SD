af96f83733f2628ba365f705ebba239c5fed8222
ErnestoVilla24 <143679022+ErnestoVilla24@users.noreply.github.com>
2024-04-20 17:32:21 -0400

    Delete src directory

--- src/metrics.py
+++
@@ -1,8 +0,0 @@
-def save_metrics(cache_type, policy, hits, misses):
-    filename = f"{cache_type}_{policy}_metrics.txt"
-    with open(filename, "w") as file:
-        file.write(f"Tipo de caché: {cache_type}\n")
-        file.write(f"Política de reemplazo: {policy}\n")
-        file.write(f"Número de aciertos: {hits}\n")
-        file.write(f"Número de fallos: {misses}\n")
-        file.write("\n")
